3f6b7f419e6223f41e233172a10e109bac8df32f
Tumbo Lobi John <johnlobiit@gmail.com>
2023-04-24 11:05:18 +0300

    created the view function for the api endpoint

--- core/views.py
+++ core/views.py
@@ -19,3 +19,24 @@ def get_price(url):
     # Convert the price to a float and return it
     price_float = float(price_string)
     return price_float
+
+
+# Define a view function for the API endpoint
+@api_view(["POST"])
+def notify_price(request):
+    # Get the data from the request body as a JSON object
+    data = request.data
+    # Get the amazon URL and the desired price from the data
+    url = data["url"]
+    desired_price = data["desired_price"]
+    # Get the current price from the amazon URL using the scraping function
+    current_price = get_price(url)
+    # Compare the current price and the desired price
+    if current_price <= desired_price:
+        # If the current price is lower or equal to the desired price, send a notification message
+        message = f"The price of {url} has dropped to {current_price}. Buy it now!"
+    else:
+        # If the current price is higher than the desired price, send a waiting message
+        message = f"The price of {url} is still {current_price}. Wait for it to drop below {desired_price}."
+    # Return a JSON response with the message
+    return Response({"message": message})
