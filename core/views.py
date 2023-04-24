473c3e3ad69dabbb55829896cb0641a15a981d43
Tumbo Lobi John <johnlobiit@gmail.com>
2023-04-24 11:09:20 +0300

    send email notification to a user

--- core/views.py
+++ core/views.py
@@ -1,9 +1,13 @@
+# Import the required modules
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from bs4 import BeautifulSoup
 import requests
+# Import the email modules
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 # Define a function to scrape the price from the amazon URL
@@ -26,17 +30,24 @@ def get_price(url):
 def notify_price(request):
     # Get the data from the request body as a JSON object
     data = request.data
-    # Get the amazon URL and the desired price from the data
+    # Get the amazon URL, the desired price and the user email from the data
     url = data["url"]
     desired_price = data["desired_price"]
+    user_email = data["user_email"]
     # Get the current price from the amazon URL using the scraping function
     current_price = get_price(url)
     # Compare the current price and the desired price
     if current_price <= desired_price:
-        # If the current price is lower or equal to the desired price, send a notification message
+        # If the current price is lower or equal to the desired price, send an email notification to the user
+        subject = f"Price Alert: {url}"
         message = f"The price of {url} has dropped to {current_price}. Buy it now!"
+        from_email = settings.EMAIL_HOST_USER  # The email address of the sender (configured in settings.py)
+        recipient_list = [user_email]  # The email address of the receiver (passed in the request data)
+        send_mail(subject, message, from_email, recipient_list)  # Send the email using Django's built-in function
+        # Also send a notification message as a JSON response
+        message = f"An email notification has been sent to {user_email}."
     else:
-        # If the current price is higher than the desired price, send a waiting message
+        # If the current price is higher than the desired price, send a waiting message as a JSON response
         message = f"The price of {url} is still {current_price}. Wait for it to drop below {desired_price}."
     # Return a JSON response with the message
     return Response({"message": message})
