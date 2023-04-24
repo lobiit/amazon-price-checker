87cb268a89ad66ddc7f52dbac0760a7ef617f787
Tumbo Lobi John <johnlobiit@gmail.com>
2023-04-24 13:08:23 +0300

    bug fixes using try and catch

---
+++ core/serializers.py
@@ -0,0 +1,16 @@
+# Import the serializers module from rest_framework
+from django.db.migrations import serializer
+from rest_framework import serializers
+
+
+# Define a serializer class for the notify price api
+class NotifyPriceSerializer(serializers.Serializer):
+    # Define a field for the amazon URL as a URLField with a label
+    url = serializers.URLField(max_length=200, required=True, label="Amazon URL")
+    # Define a field for the desired price as a DecimalField with a label
+    desired_price = serializers.DecimalField(max_digits=10, decimal_places=2, required=True, label="Desired Price")
+    # Define a field for the user email as an EmailField with a label
+    user_email = serializers.EmailField(max_length=100, required=True, label="User Email")
+    # Define a field for the content as a CharField with a label
+    content = serializers.CharField(max_length=500, required=False, label="Content")
+
--- core/views.py
+++ core/views.py
@@ -5,10 +5,13 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from bs4 import BeautifulSoup
 import requests
-# Import the sendgrid library and the model for the price alert
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+# Import the send_mail function and the model and the serializer for the price alert
+from django.core.mail import send_mail
+
+from Amazon_Price_Checker import settings
 from .models import PriceAlert
+from .serializers import NotifyPriceSerializer
+
 
 # Define a function to scrape the price from the amazon URL
 def get_price(url):
@@ -29,50 +32,51 @@ def get_price(url):
         # For example, raise an exception or return a default value
         raise ValueError("Could not find price element")
 
+
 # Define a view function for the API endpoint
 @api_view(["POST"])
 def notify_price(request):
     # Get the data from the request body as a JSON object
     data = request.data
-    # Get the amazon URL, the desired price and the user email from the data
-    url = data["url"]
-    desired_price = data["desired_price"]
-    user_email = data["user_email"]
-    # Try to get the current price from the amazon URL using the scraping function
-    try:
-        current_price = get_price(url)
-        # Compare the current price and the desired price
-        if current_price <= desired_price:
-            # If the current price is lower or equal to the desired price, send an email notification to the user using sendgrid api
-            subject = f"Price Alert: {url}"
-            message = f"The price of {url} has dropped to {current_price}. Buy it now!"
-            from_email = settings.EMAIL_HOST_USER # The email address of the sender (configured in settings.py)
-            recipient_list = [user_email] # The email address of the receiver (passed in the request data)
-            email = Mail(
-                from_email=from_email,
-                to_emails=user_email,
-                subject=subject,
-                plain_text_content=message
+    # Create an instance of the serializer with the data
+    serializer = NotifyPriceSerializer(data=data)
+    # Validate the data using the serializer
+    if serializer.is_valid():
+        # Get the amazon URL, the desired price, the user email and the content from the validated data
+        url = serializer.validated_data["url"]
+        desired_price = serializer.validated_data["desired_price"]
+        user_email = serializer.validated_data["user_email"]
+        content = serializer.validated_data.get("content", "")
+        # Try to get the current price from the amazon URL using the scraping function
+        try:
+            current_price = get_price(url)
+            # Compare the current price and the desired price
+            if current_price <= desired_price:
+                # If the current price is lower or equal to the desired price, send an email notification to the user using django mail
+                subject = f"Price Alert: {url}"
+                message = f"The price of {url} has dropped to {current_price}. Buy it now!\n{content}"  # Add the content to the message
+                from_email = settings.EMAIL_HOST_USER  # The email address of the sender (configured in settings.py)
+                recipient_list = [user_email]  # The email address of the receiver (passed in the request data)
+                send_mail(subject, message, from_email,
+                          recipient_list)  # Send the email using Django's built-in function
+
+                # Also send a notification message as a JSON response
+                message = f"An email notification has been sent to {user_email}."
+            else:
+                # If the current price is higher than the desired price, send a waiting message as a JSON response
+                message = f"The price of {url} is still {current_price}. Wait for it to drop below {desired_price}."
+            # Create or update a PriceAlert object with the validated data and save it to the database
+            price_alert, created = PriceAlert.objects.update_or_create(
+                url=url,
+                user_email=user_email,
+                defaults={"desired_price": desired_price}
             )
-            try:
-                sg = SendGridAPIClient(SENDGRID_API_KEY)
-                response = sg.send(email)
-            except Exception as e:
-                print(e.message)
-            # Also send a notification message as a JSON response
-            message = f"An email notification has been sent to {user_email}."
-        else:
-            # If the current price is higher than the desired price, send a waiting message as a JSON response
-            message = f"The price of {url} is still {current_price}. Wait for it to drop below {desired_price}."
-        # Create or update a PriceAlert object with the data from the request and save it to the database
-        price_alert, created = PriceAlert.objects.update_or_create(
-            url=url,
-            user_email=user_email,
-            defaults={"desired_price": desired_price}
-        )
-        price_alert.save()
-    except ValueError:
-        # If there was a ValueError exception raised by get_price, send an error message as a JSON response
-        message = f"Could not find price element for {url}. Please make sure it is a valid amazon product page."
-    # Return a JSON response with the message
-    return Response({"message": message})
+            price_alert.save()
+        except ValueError:
+            # If there was a ValueError exception raised by get_price, send an error message as a JSON response
+            message = f"Could not find price element for {url}. Please make sure it is a valid amazon product page."
+        # Return a JSON response with the message
+        return Response({"message": message})
+    else:
+        # If the data is not valid, return a JSON response with the errors from the serializer
+        return Response(serializer.errors)
