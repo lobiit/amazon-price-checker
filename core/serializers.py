5df94b050a907125085cb8b2d40553ae671c40b0
Tumbo Lobi John <johnlobiit@gmail.com>
2023-05-05 12:16:49 +0300

    fixed the bud in the serializer

--- core/serializers.py
+++ core/serializers.py
@@ -6,7 +6,7 @@ from rest_framework import serializers
 # Define a serializer class for the notify price api
 class NotifyPriceSerializer(serializers.Serializer):
     # Define a field for the amazon URL as a URLField with a label
-    url = serializers.URLField(max_length=500, required=True, label="Amazon URL")
+    url = serializers.URLField(max_length=700, required=True, label="Amazon URL")
     # Define a field for the desired price as a DecimalField with a label
     desired_price = serializers.DecimalField(max_digits=10, decimal_places=2, required=True, label="Desired Price")
     # Define a field for the user email as an EmailField with a label
